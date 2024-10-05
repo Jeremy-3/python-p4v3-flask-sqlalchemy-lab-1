5e03ab472dd3bedf7a7d6a3eea98b83134061551
+ <+>
2024-10-05 11:12:40 +0300

    lab done

--- server/app.py
+++ server/app.py
@@ -21,7 +21,34 @@ def index():
     return make_response(body, 200)
 
 # Add views here
-
-
+@app.route('/earthquakes/<int:id>')
+def view(id):
+   earthquake = db.session.get(Earthquake,id)
+   if earthquake:
+       res=make_response(
+           {
+               "id":earthquake.id,
+               "location":earthquake.location,
+               "magnitude":earthquake.magnitude,
+               "year":earthquake.year
+           },
+           200,
+       )
+   else:
+       res=make_response({"message":f"Earthquake {id} not found."},404)
+   return res
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def find_magnitudes(magnitude):
+    magnitudes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    res = {
+        "count": len(magnitudes),
+        "quakes": [{
+            "id": eqs.id,
+            "location": eqs.location,
+            "magnitude": eqs.magnitude,
+            "year": eqs.year
+        } for eqs in magnitudes]
+    }
+    return make_response(res,200)
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
